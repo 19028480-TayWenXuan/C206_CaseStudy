a3962770c6c8effcc196b3fab03d458f36c06fb7
19028480 <19028480@DESKTOP-7D2Q9IP>
2020-08-14 15:48:28 +0800

    BikeShop?

---
+++ C206_CaseStudy/src/BikeShop.java
@@ -0,0 +1,15 @@
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 19028480, 14 Aug 2020 3:47:45 pm
+ */
+
+/**
+ * @author 19028480
+ *
+ */
+public class BikeShop {
+
+}
