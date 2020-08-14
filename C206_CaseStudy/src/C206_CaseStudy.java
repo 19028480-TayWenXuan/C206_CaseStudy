1f2b3b16880627a845fd634e3b1334be19d26e57
19028480 <19028480@DESKTOP-7D2Q9IP>
2020-08-14 15:46:19 +0800

    test

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,9 +1,24 @@
-
 public class C206_CaseStudy {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+	}
+	
+	public static void showMainMenu() {
+		C206_CaseStudy.setHeader("Main Menu");
+		System.out.println("View Bike");
+		System.out.println("View Bike Parts");
+		System.out.println("Book Appointment");
+		System.out.println("View Appointment");
+		
+	}
 
+	private static void setHeader(String header) {
+		Helper.line(80, "-");
+		System.out.println(header);
+		Helper.line(80, "-");
+		
 	}
 
 }
