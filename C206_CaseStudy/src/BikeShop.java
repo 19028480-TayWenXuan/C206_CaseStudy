669cd7af94828a3642f0461ff13d0d652de096be
19028480 <19028480@DESKTOP-7D2Q9IP>
2020-08-14 15:48:39 +0800

    BikeShop

--- C206_CaseStudy/src/BikeShop.java
+++ C206_CaseStudy/src/BikeShop.java
@@ -1,15 +1,24 @@
-/**
- * I declare that this code was written by me.
- * I will not copy or allow others to copy my code.
- * I understand that copying code is considered as plagiarism.
- *
- * 19028480, 14 Aug 2020 3:47:45 pm
- */
-
-/**
- * @author 19028480
- *
- */
 public class BikeShop {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static void showMainMenu() {
+		BikeShop.setHeader("Main Menu");
+		System.out.println("View Bike");
+		System.out.println("View Bike Parts");
+		System.out.println("Book Appointment");
+		System.out.println("View Appointment");
+		
+	}
+
+	private static void setHeader(String header) {
+		Helper.line(80, "-");
+		System.out.println(header);
+		Helper.line(80, "-");
+		
+	}
 
 }
