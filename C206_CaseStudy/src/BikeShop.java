408206bb54c87380416411d9db4174a4654dd27e
19008161 <19008161@DESKTOP-D7G6NCL>
2020-08-19 19:30:02 +0800

    Julie commited a buyer's info.

--- C206_CaseStudy/src/BikeShop.java
+++ C206_CaseStudy/src/BikeShop.java
@@ -12,6 +12,7 @@ public class BikeShop {
 				System.out.println("Book Appointment");
 				System.out.println("View Appointment");
 				System.out.println("Quit");
+				
 				System.out.println("Buyer's information"); 
 			}
 
