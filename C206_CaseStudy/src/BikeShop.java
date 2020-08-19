7e1ef47d3f0d008321b7819ff507fdb6e01d7552
19008161 <19008161@DESKTOP-D7G6NCL>
2020-08-19 19:09:45 +0800

    Julie added Buyer's information in line 15.

---
+++ C206_CaseStudy/src/BikeShop.java
@@ -0,0 +1,28 @@
+public class BikeShop {
+			
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+
+			}
+			
+			public static void showMainMenu() {
+				BikeShop.setHeader("Main Menu");
+				System.out.println("View Bike");
+				System.out.println("View Bike Parts");
+				System.out.println("Book Appointment");
+				System.out.println("View Appointment");
+				System.out.println("Quit");
+				System.out.println("Buyer's information");
+			}
+
+			private static void setHeader(String header) {
+				Helper.line(80, "-");
+				System.out.println(header);
+				Helper.line(80, "-");
+				
+			}
+
+		}
+	
+
+		
