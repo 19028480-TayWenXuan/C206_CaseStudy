2fb91f9ddb26dd564e2c9b6fd200cf20fa8d3194
19008161 <19008161@DESKTOP-D7G6NCL>
2020-08-19 21:51:28 +0800

    menu options

--- C206_CaseStudy/src/BikeShop.java
+++ C206_CaseStudy/src/BikeShop.java
@@ -6,13 +6,13 @@ public class BikeShop {
 	}
 	
 	public static void showMainMenu() {
-		BikeShop.setHeader("1. Main Menu");
-		System.out.println("2. View Bike");
-		System.out.println("3. View Bike Parts");
-		System.out.println("4. Book Appointment");
-		System.out.println("5. View Appointment");
-		System.out.println("6. Buyer's information");
-		System.out.println("7. Quit");
+		BikeShop.setHeader("Main Menu");
+		System.out.println("1. View Bike");
+		System.out.println("2. View Bike Parts");
+		System.out.println("3. Book Appointment");
+		System.out.println("4. View Appointment");
+		System.out.println("5. Buyer's information");
+		System.out.println("6. Quit");
 		
 		
 	}
