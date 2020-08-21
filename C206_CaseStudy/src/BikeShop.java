b5792e00c857b8670b83a4676ccfa7261605230e
19028480-TayWenXuan <68407638+19028480-TayWenXuan@users.noreply.github.com>
2020-08-21 10:54:05 +0800

    Update BikeShop.java

--- C206_CaseStudy/src/BikeShop.java
+++ C206_CaseStudy/src/BikeShop.java
@@ -14,7 +14,7 @@ public class BikeShop {
 		 ArrayList<Bike> bikeList = new ArrayList<Bike>();
 		int option = 0;
 
-		while (option != 6) {
+		while (option != 7) {
 			showMainMenu();
 			option = Helper.readInt("Enter option > ");
 
@@ -32,7 +32,7 @@ public class BikeShop {
 					BikeShop.delBike(bikeList);
 				}
 				
-			}
+		}
 			
 			else if(option == 2) {
 				Helper.line(20, "-");
@@ -93,6 +93,25 @@ public class BikeShop {
 				}
 			}
 		}
+			//menu for member5
+		        else if(option == 6){
+				ShowFeedbackMenu();
+				int choice = Helper.readInt("Enter your choice:");
+				if (choice == 1) {
+					BikeShop.addFeedback(feedbackArrayList);
+				}
+				else if (choice == 2) {
+					BikeShop.viewAllFeedback(feedbackArrayList);
+				}
+				else if (choice == 3) {
+					BikeShop.delFeedback(feedbackArrayList);
+				}
+			}
+	        }
+					
+				
+		
+		       
 	}
 				
 	//member1 delete method
@@ -179,6 +198,13 @@ public class BikeShop {
 		System.out.println("3. Delete Bike");
 	}
 	
+	// member 5 Feedback Menu
+	private static void ShowFeedbackMenu() {
+		System.out.println("1. Add Feedback");
+		System.out.println("2. View All Feedbacks");
+		System.out.println("3. Delete Feedbacks");
+	}
+	
 	//main menu
 	public static void showMainMenu() {
 		BikeShop.setHeader("Main Menu");
@@ -187,7 +213,8 @@ public class BikeShop {
 		System.out.println("3. Book Appointment");
 		System.out.println("4. View Appointment");
 		System.out.println("5. Buyer's information");
-		System.out.println("6. Quit");
+		System.out.println("6. Feedbacks");
+		System.out.println("7. Quit");
 	}
 
 	private static void setHeader(String header) {
